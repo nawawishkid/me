752ea8854d427d573fdf4035e3b46bd5c34156b5
nawawishkid <nawawish.samerpark@gmail.com>
2023-04-10 01:58:36 +0700

    Add projects section

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "roomai.design",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,67 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { motion } from "framer-motion";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
+const projectList: {
+  title: string;
+  description: string;
+  logoUrl: string;
+  url: string;
+}[] = [
+  {
+    title: "RoomAI.design",
+    description: "Transform Your Space with AI-Powered Interior Design",
+    url: "https://roomai.design/",
+    logoUrl: "https://roomai.design/logo.png",
+  },
+];
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Heading />
-        <motion.p
-          animate={{ opacity: [0, 1] }}
-          transition={{ delay: 3.5, duration: 1 }}
-          className="opacity-0"
-        >
-          I make some stuff
-        </motion.p>
+    <main className="min-h-screen">
+      <div className="p-8 flex justify-center items-center">
+        <div className="my-24">
+          <Heading />
+          <motion.p
+            animate={{ opacity: [0, 1] }}
+            transition={{ delay: 3.5, duration: 1 }}
+            className="opacity-0"
+          >
+            I make some stuff
+          </motion.p>
+        </div>
+      </div>
+      <div className="my-8 p-8 max-w-screen-lg flex items-center flex-col mx-auto">
+        <h2 className="text-3xl mb-16">My projects</h2>
+        <ul>
+          {projectList.map((project) => (
+            <li key={project.url} className="mb-4">
+              <div className="p-4 rounded-lg border flex items-start gap-4">
+                <Image
+                  src={project.logoUrl}
+                  width={64}
+                  height={64}
+                  alt={`${project.title}'s logo`}
+                />
+                <div>
+                  <div className="mb-4">
+                    <h3 className="text-2xl font-bold">{project.title}</h3>
+                    <Link
+                      href={project.url}
+                      target="_blank"
+                      className="underline text-slate-500"
+                    >
+                      <small>{new URL(project.url).hostname}</small>
+                    </Link>
+                  </div>
+                  <p>{project.description}</p>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </main>
   );
