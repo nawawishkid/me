913cec6348f30df8e58e0cc10c19b19f8a150ebf
nawawishkid <nawawish.samerpark@gmail.com>
2023-04-13 02:14:44 +0700

    Fix layout issues

--- src/app/blogs/[slug]/loading.tsx
+++ src/app/blogs/[slug]/loading.tsx
@@ -3,7 +3,7 @@ import Animatable from "../../../components/animatable";
 export default function Loading() {
   return (
     <>
-      <Animatable className="max-w-screen-lg p-8">
+      <Animatable className="max-w-screen-lg">
         <div className="w-1/2 bg-slate-200 animate-pulse h-12 rounded mx-auto my-8"></div>
         <div className="mb-2 w-full h-4 bg-slate-200 animate-pulse rounded"></div>
         <div className="mb-2 w-full h-4 bg-slate-200 animate-pulse rounded"></div>
