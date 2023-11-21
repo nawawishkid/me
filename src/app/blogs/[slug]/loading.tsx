82e929bd66956445e79562cf31f6b8b976fa3543
nawawishkid <nawawish.samerpark@gmail.com>
2023-11-21 14:35:02 +0700

    Adjust loading skeleton style

--- src/app/blogs/[slug]/loading.tsx
+++ src/app/blogs/[slug]/loading.tsx
@@ -1,9 +1,9 @@
-import Animatable from "../../../components/animatable";
+import Animatable from "@/components/animatable";
 
 export default function Loading() {
   return (
     <>
-      <Animatable className="max-w-screen-lg">
+      <Animatable className="max-w-screen-md mx-auto">
         <div className="w-1/2 bg-slate-200 animate-pulse h-12 rounded mx-auto my-8"></div>
         <div className="mb-2 w-full h-4 bg-slate-200 animate-pulse rounded"></div>
         <div className="mb-2 w-full h-4 bg-slate-200 animate-pulse rounded"></div>
