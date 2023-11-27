118d9b9da6c3f61610ba9f16e745d3303cc8874c
nawawishkid <nawawish.samerpark@gmail.com>
2023-11-27 12:59:38 +0700

    Add site development notice

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,20 +4,23 @@ import Nav from "./nav";
 
 export default function Header() {
   return (
-    <div className="h-16">
-      <header
-        className={`fixed flex items-center justify-between gap-4 left-0 w-full h-16 z-50 bg-slate-50 top-0`}
-      >
-        <Link href="/" className="inline-flex items-center gap-4 p-4">
-          <Image
-            src="/logo.png"
-            width={32}
-            height={32}
-            alt="Nawawishkid's logo"
-          />
-          nawawishkid.me
-        </Link>
-        <Nav />
+    <div className="h-[88px]">
+      <header className="fixed left-0 w-full z-50 bg-slate-50 top-0">
+        <div className="flex justify-center items-center bg-red-200 text-red-500 text-xs h-6">
+          This site is in development
+        </div>
+        <div className={`flex items-center justify-between gap-4`}>
+          <Link href="/" className="inline-flex items-center gap-4 p-4">
+            <Image
+              src="/logo.png"
+              width={32}
+              height={32}
+              alt="Nawawishkid's logo"
+            />
+            nawawishkid.me
+          </Link>
+          <Nav />
+        </div>
       </header>
     </div>
   );
